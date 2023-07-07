cbca202f0f8362dc6a56fdd1fbf9ec0bff551f24
ShaheerKz <shariikhan925@gmail.com>
2023-07-07 15:55:11 +0500

    feat: second class work

---
+++ javascript-practice/operator.js
@@ -0,0 +1,37 @@
+document.write("<h1>" + "Operators Practice" + "</h1>");
+// Addidion
+let a = "Shaheer";
+let b = "Khanzada";
+let c = a + " " + b;
+// Substraction
+let d = 10;
+let e = 5;
+let f = d - e;
+// Multiplication
+let g = 200;
+let h = g * 2;
+// equal value
+let i = "10";
+let j = 10;
+let k = i == j;
+// equal value or equal type
+let l = "10";
+let m = 10;
+let n = i === j;
+// greater then
+let o = 100 > 50;
+// less then
+let p = 50 < 100;
+// greater then or equals to
+let q = 40 >= 50;
+// less then or equals to
+let r = 40 <= 50;
+console.log(c);
+console.log(f);
+console.log(h);
+console.log(k);
+console.log(n);
+console.log(o);
+console.log(p);
+console.log(q);
+console.log(r);
